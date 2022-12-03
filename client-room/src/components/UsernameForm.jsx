1cbb213e21ffab55149bb1dab0e4b0c4e3b0f21f
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-12-04 05:50:45 +0600

    client side form for room complete

--- client-room/src/components/UsernameForm.jsx
+++ client-room/src/components/UsernameForm.jsx
@@ -1,7 +1,16 @@
-const Form = () => {
-  <div>
-    Form
-  </div>
+import React from 'react'
+
+const Form = (props) => {
+  return(
+    <form>
+      <input
+        placeholder='Username...'
+        type="text"
+        value={props.username}
+        onChange={props.onChange}
+      />
+    </form>
+  )
 }
 
 export default Form
